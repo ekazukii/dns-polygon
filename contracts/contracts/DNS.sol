d357afe4399d015aa21c1bf7e4d8c444917f0d2e
Baptiste Loison <baptisteloison8400@gmail.com>
2022-06-10 14:35:24 +0200

    Add comments to Domains contract

--- contracts/contracts/DNS.sol
+++ contracts/contracts/DNS.sol
@@ -26,33 +26,43 @@ enum RecordType {
 }
 
 contract Domains is ERC721 {
-    //mapping(string => address) public domains;
-    //mapping(string => string) public avatars;
 
+    // Bind all domains name to the records
     mapping(string => Record) public records;
+
+    // Bind all NFTs ID to the domains name
     mapping(uint => string) public names;
+    // Now in the other direction, domain name to NFT ID
     mapping(string => uint) public ids;
 
+    // Store the top level domain string
     string public tld;
+    // Owner of the contract
     address payable public owner;
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    // Static parts of the NFT's default avatar. Need to add dynamically the name of the domain
     string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"> <path fill="url(#B)" d="M0 0h270v270H0z"/><defs><filter id="A" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M72.863 42.949c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-10.081 6.032-6.85 3.934-10.081 6.032c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-8.013-4.721a4.52 4.52 0 0 1-1.589-1.616c-.384-.665-.594-1.418-.608-2.187v-9.31c-.013-.775.185-1.538.572-2.208a4.25 4.25 0 0 1 1.625-1.595l7.884-4.59c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v6.032l6.85-4.065v-6.032c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595L41.456 24.59c-.668-.387-1.426-.59-2.197-.59s-1.529.204-2.197.59l-14.864 8.655a4.25 4.25 0 0 0-1.625 1.595c-.387.67-.585 1.434-.572 2.208v17.441c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l10.081-5.901 6.85-4.065 10.081-5.901c.668-.387 1.426-.59 2.197-.59s1.529.204 2.197.59l7.884 4.59a4.52 4.52 0 0 1 1.589 1.616c.384.665.594 1.418.608 2.187v9.311c.013.775-.185 1.538-.572 2.208a4.25 4.25 0 0 1-1.625 1.595l-7.884 4.721c-.668.387-1.426.59-2.197.59s-1.529-.204-2.197-.59l-7.884-4.59a4.52 4.52 0 0 1-1.589-1.616c-.385-.665-.594-1.418-.608-2.187v-6.032l-6.85 4.065v6.032c-.013.775.185 1.538.572 2.208a4.25 4.25 0 0 0 1.625 1.595l14.864 8.655c.668.387 1.426.59 2.197.59s1.529-.204 2.197-.59l14.864-8.655c.657-.394 1.204-.95 1.589-1.616s.594-1.418.609-2.187V55.538c.013-.775-.185-1.538-.572-2.208a4.25 4.25 0 0 0-1.625-1.595l-14.993-8.786z" fill="#FFF"/><defs><linearGradient id="B" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#b033d6"/><stop offset="1" stop-color="#e0961d" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#A)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
     string svgPartTwo = '</text></svg>';
 
+    // Thrown when user don't own the domain
     error Unauthorized();
+    // Thrown when domain is already owned
     error AlreadyRegistered();
+    // Throw when domain has an invalid name (too short, too long, ...)
     error InvalidName(string name);
 
-   constructor(string memory _tld) payable ERC721("Hodl Name Service", "HNS") {
+    // We init the NFT with the owner of the contract and set the first token ID to 1
+    constructor(string memory _tld) payable ERC721("Hodl Name Service", "HNS") {
        owner = payable(msg.sender);
         tld = _tld;
         console.log("%s name service deployed", _tld);
         _tokenIds.increment();
     }
 
+    // Check if the sender is the contract owner
     function isOwner() public view returns (bool) {
         return msg.sender == owner;
     }
@@ -62,6 +72,7 @@ contract Domains is ERC721 {
         _;
     }
 
+    // Withdraw the money in the contract
     function withdraw() public onlyOwner {
         uint amount = address(this).balance;
         
@@ -69,6 +80,8 @@ contract Domains is ERC721 {
         require(success, "Failed to withdraw Matic");
     } 
 
+    // Return all the domains name registered in the contract
+    // Can be used to display all domains on a website or make a search
     function getAllNames() public view returns (string[] memory) {
         console.log("Getting all names from contract");
         string[] memory allNames = new string[](_tokenIds.current()-1);
@@ -80,10 +93,12 @@ contract Domains is ERC721 {
         return allNames;
     }
 
+    // Check if the domain name is valid
     function valid(string calldata name) public pure returns(bool) {
         return StringUtils.strlen(name) >= 3 && StringUtils.strlen(name) <= 12;
     }
 
+    // Calculate the domain name price based on the length of the name
     function price(string calldata name) public pure returns(uint) {
         uint len = StringUtils.strlen(name);
         require(len > 0);
@@ -95,7 +110,8 @@ contract Domains is ERC721 {
 	        return 1 * 10**17;
         }
     }
-  	
+    
+    // Pay to register a new domain. Check if the name is available, valid and if the sender has enough money
 	function register(string calldata name) public payable {
         if (ids[name] != 0) revert AlreadyRegistered();
         if (!valid(name)) revert InvalidName(name);
@@ -114,6 +130,12 @@ contract Domains is ERC721 {
         _tokenIds.increment();
     }
 
+    // Return the NFT uri for the given token ID
+    // The metadata contains:
+    //   - The name of the domain with the TLD
+    //   - The description of the NFT
+    //   - The image of the NFT, by default it's an SVG with the name of the domain
+    //   - The length of the name of the domain
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(isSet(names[tokenId]), "Address unknown");
 
@@ -151,6 +173,8 @@ contract Domains is ERC721 {
         return string( abi.encodePacked("data:application/json;base64,", json));
     }
 
+    // Return NFT id for the given domain name
+    // Used to get metadata from an domain name 
     function getId(string calldata name) public view returns(uint) {
         require(ids[name] != 0);
         return ids[name];
@@ -161,6 +185,7 @@ contract Domains is ERC721 {
        return ownerOf(getId(name));
     }
 
+    // Set one record for the given domain name
     function setRecord(string calldata name, string calldata record, RecordType recordType) public {
 		// Check that the owner is the transaction sender
         if (msg.sender != getAddress(name)) revert Unauthorized();
@@ -178,6 +203,7 @@ contract Domains is ERC721 {
         }
     }
 
+    // Set multiple records for the given domain name.
     // One string is in memory cause https://forum.openzeppelin.com/t/stack-too-deep-when-compiling-inline-assembly/11391/4
     function setRecords(string calldata name, string memory _avatar, string calldata _twitterTag, string calldata _website, string calldata _email, string calldata _description) public {
         if (msg.sender != getAddress(name)) revert Unauthorized();
@@ -189,6 +215,7 @@ contract Domains is ERC721 {
         records[name].description = _description;
     }
 
+    // Get a specific record for the given domain name
     function getRecord(string calldata name, RecordType recordType) public view returns(string memory) {
         if(recordType == RecordType.AVATAR) {
             return records[name].avatar;
@@ -205,6 +232,7 @@ contract Domains is ERC721 {
         revert("Record not found");
     }
 
+    // Get all the records for the given domain name
     function getRecords(string calldata name) public view returns(string[] memory, address) {
         address addr = getAddress(name);
         string[] memory allRecords = new string[](5);
@@ -218,6 +246,7 @@ contract Domains is ERC721 {
         return (allRecords, addr);
     }
 
+    // Check if string isn't empty
     function isSet(string memory name) public pure returns(bool) {
         return StringUtils.strlen(name) != 0;
     }
