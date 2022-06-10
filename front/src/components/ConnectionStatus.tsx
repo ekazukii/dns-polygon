24cca192219fd5ce04b69c1f415f24f5ad41950d
Baptiste Loison <baptisteloison8400@gmail.com>
2022-06-10 15:40:28 +0200

    Show the current domain name of account

--- front/src/components/ConnectionStatus.tsx
+++ front/src/components/ConnectionStatus.tsx
@@ -3,9 +3,25 @@ import { useWeb3React } from '@web3-react/core';
 import polygonLogo from '../assets/polygonlogo.png';
 import ethLogo from '../assets/ethlogo.png';
 import { networks } from '../utils/networks';
+import { useEffect, useState } from 'react';
+import contractABI from '../utils/contractABI.json';
+import { ethers } from 'ethers';
+
+const REVERSE_ADDRESS = '0x73D38666dF4F165189cb8124Be1Ac636a5bCe8b5';
 
 export default function ConnectionStatus() {
-  const { account, chainId } = useWeb3React();
+  const { account, chainId, library } = useWeb3React();
+  const [name, setName] = useState<undefined | string>();
+
+  useEffect(() => {
+    if (!account || !library) return;
+    console.log('présent');
+    const signer = library.getSigner();
+    const contract = new ethers.Contract(REVERSE_ADDRESS, contractABI.reverseAbi, signer);
+    contract.resolve(account).then((name: any) => {
+      setName(name + '.hold');
+    });
+  }, [account, library]);
 
   return (
     <div className="right">
@@ -19,7 +35,13 @@ export default function ConnectionStatus() {
       />
       {account ? (
         <p>
-          Wallet: {account.slice(0, 6)}...{account.slice(-4)}{' '}
+          {name ? (
+            <>Wallet: {name}</>
+          ) : (
+            <>
+              Wallet: {account.slice(0, 6)}...{account.slice(-4)}
+            </>
+          )}{' '}
         </p>
       ) : (
         <p> Not connected </p>
